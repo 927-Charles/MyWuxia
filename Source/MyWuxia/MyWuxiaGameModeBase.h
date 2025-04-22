85ceef1fd93e4894fc5af2251eab6b2d96e00c58
xu jingchao <2116583183@qq.com>
2025-04-22 09:38:09 +0800

    Initial commit: UE 项目基础版本

---
+++ Source/MyWuxia/MyWuxiaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyWuxiaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYWUXIA_API AMyWuxiaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
